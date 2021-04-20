c891c32266848d004f5bc7b2f2e7ee74e6d3f134
Fernando Garbo <nando.garbo@gmail.com>
2021-04-20 19:39:43 -0300

    Studying requests and responses

---
+++ Projeto01_Conceitos/src/br/com/portoseguro/servlets/LoginServlet.java
@@ -0,0 +1,45 @@
+package br.com.portoseguro.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Login Servlet", urlPatterns = { "/login" })
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("login.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			//obtendo os dados de entrada do usuario
+			String usuario = request.getParameter("txtUsuario");
+			String password = request.getParameter("txtSenha");
+			
+			if (usuario.equals("admin") && password.equals("admin")) {
+				//validado
+				//response.sendRedirect("resultado.jsp");
+				request.setAttribute("mensagem", "Bem vindo, usuário autenticado.");
+			}else {				
+				//invalido
+				request.setAttribute("mensagem", "Erro: Usuário ou senha inválidos.");
+			}
+			
+		} catch (Exception e) {
+			throw e;
+		}
+		finally {
+			request.getRequestDispatcher("resultado.jsp").forward(request, response);;
+		}
+	}
+
+}
